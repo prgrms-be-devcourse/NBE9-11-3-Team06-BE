ba23f34a50da49c0722d7ae2320be34df6b97347
k-equals0 <sallykey@naver.com>
2026-05-15 17:44:15 +0900

    refactor: migrate Participant Entity to Kotlin

---
+++ src/main/java/com/back/nbe9112team06/domain/participant/entity/Participant.kt
@@ -0,0 +1,30 @@
+package com.back.nbe9112team06.domain.participant.entity
+
+import com.back.nbe9112team06.domain.meeting.entity.Meeting
+import com.back.nbe9112team06.global.entity.BaseEntity
+import jakarta.persistence.*
+
+@Entity
+class Participant(
+    @Column(name = "guest_name")
+    var guestName: String,
+
+    @Column(name = "guest_password")
+    var guestPassword: String
+) : BaseEntity() {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "meeting_id")
+    var meeting: Meeting? = null
+
+    fun assignMeeting(meeting: Meeting) {
+        this.meeting = meeting
+    }
+
+    companion object {
+        @JvmStatic
+        fun create(guestName: String, guestPassword: String): Participant {
+            return Participant(guestName, guestPassword)
+        }
+    }
+}
